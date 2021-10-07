72dd9b0b4716e417ae2dcf726d493980ec0353eb
Diego Schernoski <dschernoski@bionexo.com>
2021-10-07 17:34:35 -0300

    20211007

---
+++ ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank.Funcionarios
+{
+    public class Diretor : Funcionario
+    {
+        public double GetBonificacao()
+        {
+            return Salario;
+        }
+
+    }
+}
